e9c91f9a9d467052a63cf8ec41ac61f1d68d67d2
tjbdev <unconfigured@null.gmail.com>
2020-05-21 20:02:24 +1000

    added multiple face functionality

--- controllers/image.js
+++ controllers/image.js
@@ -2,7 +2,7 @@ const Clarifai = require('clarifai');
 
 //You must add your own API key here from Clarifai.
 const app = new Clarifai.App({
- apiKey: 'YOUR_API_KEY_HERE'
+ apiKey: 'f4fc7971995d4cfea763dba31483d59a'
 });
 
 const handleApiCall = (req, res) => {
