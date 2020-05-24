1a22f564dccd47c5a465304eac275534b1ed8be8
tjbdev <unconfigured@null.gmail.com>
2020-05-24 10:39:32 +1000

    docker

--- controllers/image.js
+++ controllers/image.js
@@ -2,7 +2,7 @@ const Clarifai = require('clarifai');
 
 //You must add your own API key here from Clarifai.
 const app = new Clarifai.App({
- apiKey: 'f4fc7971995d4cfea763dba31483d59a'
+ apiKey: process.env.CLARIFAI_KEY
 });
 
 const handleApiCall = (req, res) => {
